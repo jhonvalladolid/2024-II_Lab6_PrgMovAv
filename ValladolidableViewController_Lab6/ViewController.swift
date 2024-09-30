9a3967a3213392c30c7d0eeeb8c2d53672d799b3
jhonvalladolid <jhonvalladolidoficial@gmail.com>
2024-09-30 10:59:28 -0500

    Initial Commit

---
+++ ValladolidableViewController_Lab6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ValladolidableViewController_Lab6
+//
+//  Created by Mac05 on 30/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
